f9162f5aa5887d48f50885962a06f582047160e8
Jose Candia <josecandiap@gmail.com>
2023-06-08 14:28:37 -0400

    se agrega contador de caracteres

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit', views.submit, name='submit')
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,3 +5,6 @@ app_name = 'blog'
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def submit(request):
+    return render(request, 'blog/submit.html')
