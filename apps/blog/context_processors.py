29444bb1d4b439addfe010fa08fa431ec121158f
Jose Candia <josecandiap@gmail.com>
2023-06-14 22:17:02 -0400

    se comenta grupos en el lado izquierdo

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -2,7 +2,7 @@ from .models import Post
 from .models import BlogGroup
 
 def global_data(request):
-    post = Post.objects.order_by('-publish_date')[:5]
+    post = Post.objects.order_by('-publish_date')[:10]
     blog_group = BlogGroup.objects.order_by('members')[:5]
     data = { 'top_post': post, 'top_groups': blog_group }
     return data
